e4bb16b8253aac3cfe06cc9ef233e07cf894d3b9
Jon Sharp <sharpjon@gmail.com>
2023-08-30 13:46:21 -0500

    Fixups for merge of mouse-pr branch

--- app/src/mouse/tick_listener.c
+++ app/src/mouse/tick_listener.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -13,7 +13,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/endpoints.h>
 #include <zmk/mouse.h>
 
-#include <sys/util.h> // CLAMP
+#include <zephyr/sys/util.h> // CLAMP
 
 #if CONFIG_MINIMAL_LIBC
 static float powf(float base, float exponent) {
